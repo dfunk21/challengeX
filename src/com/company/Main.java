ebbfdaae30a857d22b2188e80c48c67869deb297
df377 <df377@pvsd.org>
2016-11-30 14:27:35 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer();
+        //The above creates an object
+
+        String x = "";
+
+        for (int i=0; i<8; i++) {
+            sb.append("X");
+            System.out.println(sb);
+        }
+
+        //String Buffer vs. String Concatenation
+            //String Buffer is used to make programming more efficient
+
+
+    }
+}
